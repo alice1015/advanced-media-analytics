303a26867e2d46b068d63eaca9c2c5d100de85de
alice1015 <52105672+alice1015@users.noreply.github.com>
2019-07-31 12:47:11 -0400

    Add files via upload

---
+++ Join all impression files first and then match the activity log.py
@@ -0,0 +1,576 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+imps_0730_1 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073002_20190730_005431_2606873417.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[3]:
+
+
+imps_0730_2 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073003_20190730_024648_2606900306.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[4]:
+
+
+imps_0730_3 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073004_20190730_033151_2606980797.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[6]:
+
+
+imps_0730_4 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073006_20190730_041116_2607046988.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[12]:
+
+
+imps_0730_5 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073005_20190730_034827_2607011230.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[13]:
+
+
+imps_0730_6 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073007_20190730_045258_2607101994.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[9]:
+
+
+imps_0730_7 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073008_20190730_060307_2607135119.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[14]:
+
+
+imps_0730_8 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073009_20190730_070627_2607178644.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[15]:
+
+
+imps_0730_9 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073010_20190730_080538_2607216719.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[16]:
+
+
+imps_0730_10 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073011_20190730_101039_2607249883.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[17]:
+
+
+imps_0730_11 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073012_20190730_102114_2607280968.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[19]:
+
+
+imps_0730_12 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073013_20190730_115958_2607322069.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[20]:
+
+
+imps_0730_13 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073014_20190730_121127_2607363209.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[21]:
+
+
+imps_0730_14 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073015_20190730_131533_2607396023.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[23]:
+
+
+imps_0730_15 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073016_20190730_140600_2607431914.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[24]:
+
+
+imps_0730_16 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073017_20190730_153904_2607456767.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[25]:
+
+
+imps_0730_17 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073018_20190730_161157_2607483687.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[26]:
+
+
+imps_0730_18 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073019_20190730_172314_2607523354.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[27]:
+
+
+imps_0730_19 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073020_20190730_182106_2607568522.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[28]:
+
+
+imps_0730_20 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073021_20190730_192536_2607598435.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[29]:
+
+
+imps_0730_21 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073022_20190730_202357_2607642995.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[32]:
+
+
+imps_0730_22 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073023_20190730_215336_2607693343.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[34]:
+
+
+imps_0730_23 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073100_20190730_223730_2607742991.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[36]:
+
+
+imps_0730_24 = pd.read_csv('dcm_advertiser8548780_8548005_impression_2019073101_20190730_232439_2607845327.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[38]:
+
+
+imps_0730_1.shape
+
+
+# In[39]:
+
+
+imps_0730_2.shape
+
+
+# In[40]:
+
+
+imps_0730_3.shape
+
+
+# In[41]:
+
+
+imps_0730_4.shape
+
+
+# In[42]:
+
+
+imps_0730_5.shape
+
+
+# In[43]:
+
+
+imps_0730_6.shape
+
+
+# In[44]:
+
+
+imps_0730_7.shape
+
+
+# In[45]:
+
+
+imps_0730_8.shape
+
+
+# In[46]:
+
+
+imps_0730_9.shape
+
+
+# In[47]:
+
+
+imps_0730_10.shape
+
+
+# In[48]:
+
+
+imps_0730_11.shape
+
+
+# In[49]:
+
+
+imps_0730_12.shape
+
+
+# In[50]:
+
+
+imps_0730_13.shape
+
+
+# In[51]:
+
+
+imps_0730_14.shape
+
+
+# In[52]:
+
+
+imps_0730_15.shape
+
+
+# In[53]:
+
+
+imps_0730_16.shape
+
+
+# In[54]:
+
+
+imps_0730_17.shape
+
+
+# In[55]:
+
+
+imps_0730_18.shape
+
+
+# In[56]:
+
+
+imps_0730_19.shape
+
+
+# In[57]:
+
+
+imps_0730_20.shape
+
+
+# In[58]:
+
+
+imps_0730_21.shape
+
+
+# In[59]:
+
+
+imps_0730_22.shape
+
+
+# In[60]:
+
+
+imps_0730_23.shape
+
+
+# In[61]:
+
+
+imps_0730_24.shape
+
+
+# In[62]:
+
+
+#total length of the dataframe
+
+60510+62589+62065+75486+82765+102545+113940+129732+117268+118600+141758+138270+138695+122572+112483+82208+80213+80847+89133+40937+47211+39263+45518+62921
+
+
+# In[63]:
+
+
+imps_0730 = pd.concat([imps_0730_1, imps_0730_2, imps_0730_3, imps_0730_4, imps_0730_5, imps_0730_6, imps_0730_7, imps_0730_8, imps_0730_9, imps_0730_10, imps_0730_11, imps_0730_12, imps_0730_13, imps_0730_14, imps_0730_15, imps_0730_16, imps_0730_17, imps_0730_18, imps_0730_19, imps_0730_20, imps_0730_21, imps_0730_22, imps_0730_23, imps_0730_24], axis=0)
+
+
+# In[65]:
+
+
+imps_0730.shape
+
+
+# In[66]:
+
+
+imps_0730 = imps_0730.reset_index(drop=True)
+
+
+# In[67]:
+
+
+imps_0730.head(5)
+
+
+# In[70]:
+
+
+imps_0730_us_only = imps_0730['Campaign ID'] == 21503287
+
+
+# In[71]:
+
+
+imps_0730_us = imps_0730[imps_0730_us_only]
+
+
+# In[72]:
+
+
+#total impressions of the us_campaign on 07/30
+
+imps_0730_us.shape
+
+
+# In[73]:
+
+
+#how many unique users that us_campaign reached on 07/30
+
+imps_0730_us['User ID'].nunique()
+
+
+# In[75]:
+
+
+activity_0730 = pd.read_csv('dcm_advertiser8548780_8548005_activity_20190730_20190731_051401_2608059800.csv.gz', compression='gzip', header=0, sep=',', error_bad_lines = False)
+
+
+# In[76]:
+
+
+#list all headers of the activity log
+
+list(activity_0730.columns.values)
+
+
+# In[77]:
+
+
+activity_0730_us_only = activity_0730['Campaign ID'] == 21503287
+
+
+# In[78]:
+
+
+activity_0730_us = activity_0730[activity_0730_us_only]
+
+
+# In[79]:
+
+
+#how many floodlight conversions we have in US on 0730
+
+activity_0730_us.shape
+
+
+# In[82]:
+
+
+activity_0730_us_credit_only = activity_0730['Activity ID'] == 7245096
+activity_0730_us_sub1_only = activity_0730['Activity ID'] == 7305055
+activity_0730_us_sub2_only = activity_0730['Activity ID'] == 7252068
+activity_0730_us_credit = activity_0730[activity_0730_us_credit_only]
+activity_0730_us_sub1 = activity_0730[activity_0730_us_sub1_only]
+activity_0730_us_sub2 = activity_0730[activity_0730_us_sub2_only]
+
+
+# In[89]:
+
+
+activity_0730_us_trans = pd.concat([activity_0730_us_credit, activity_0730_us_sub1, activity_0730_us_sub2])
+
+
+# In[90]:
+
+
+#how many transactions happened in us on 0730
+
+activity_0730_us_trans.shape
+
+
+# In[91]:
+
+
+#count unique users who completed the transaction
+
+activity_0730_us_trans['User ID'].nunique()
+
+
+# In[92]:
+
+
+activity_0730_us_trans.head(20)
+
+
+# In[135]:
+
+
+#find subset of users who are non-0
+
+activity_0730_us_trans_known_users = activity_0730_us_trans[(activity_0730_us_trans.select_dtypes(include=['number']) != 0).any(1)]
+
+
+# In[136]:
+
+
+activity_0730_us_trans_known_users.head(5)
+
+
+# In[ ]:
+
+
+
+
+
+# In[86]:
+
+
+import numpy as np
+
+
+# In[114]:
+
+
+activity_df = pd.DataFrame(activity_0730_us_trans['User ID'], activity_0730_us_trans['Interaction Time'])
+
+
+# In[116]:
+
+
+activity_df['User ID'].dtype
+
+
+# In[115]:
+
+
+activity_df.head(20)
+
+
+# In[101]:
+
+
+activity_df = activity_df.rename(columns = {'Interaction Time': 'time_1', 'User ID': 'user_id'})
+
+
+# In[110]:
+
+
+imps_df = pd.DataFrame(imps_0730_us['Event Time'], imps_0730_us['User ID'])
+
+
+# In[119]:
+
+
+imps_df = imps_df.rename(columns = {'Event Time': 'time_1', 'User ID': 'user_id'})
+
+
+# In[120]:
+
+
+imps_df.head(5)
+
+
+# In[117]:
+
+
+imps_df.time_1.dtype
+
+
+# In[109]:
+
+
+imps_0730_us.head(5)
+
+
+# In[130]:
+
+
+activity_0730_us_trans['Interaction Time'] = activity_0730_us_trans['Interaction Time'].astype(int)
+imps_0730_us['Event Time'] = imps_0730_us['Event Time'].astype(int)
+
+
+# In[131]:
+
+
+# add a dummy column to activity_0730_us_trans
+activity_0730_us_trans['dummy_col'] = 1
+
+# merge activity_0730_us_trans and imps_0730_us where timestamps are identical
+# use a left join so when imps_0730_us and activity_0730_us_trans match on the timefield fields
+# the dummy_col will have value of 1, otherwise Null
+results = (
+    imps_0730_us
+    .merge(
+        activity_0730_us_trans[['Interaction Time', 'dummy_col']],
+        left_on='Event Time',
+        right_on='Interaction Time',
+        how='left'
+    )
+)
+
+# replace null values (i.e. those where the join did not work) with `
+results['dummy_col'] = results['dummy_col'].fillna(0)
+
+
+# In[132]:
+
+
+results.head(5)
+
+
+# In[133]:
+
+
+val = results['dummy_col'].sum() 
+print(val)
+
+
+# In[126]:
+
+
+results['Interaction Time']
+
+
+# In[128]:
+
+
+results['Event Time']
+
+
+# In[129]:
+
+
+activity_0730_us_trans['Event Time']
+
+
+# In[ ]:
+
+
+
+
